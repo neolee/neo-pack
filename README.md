c2ac4e03e8641901f3b73200e4e90aa5d8e04a26
Neo Lee <neo.lee@gmail.com>
2013-04-01 14:15:38 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## My Custom Pack for Emacs Live
+
+Migrated my .emacs to an Emacs Live user pack.
---
+++ info.el
@@ -0,0 +1,3 @@
+(live-pack-name "neo-pack")
+(live-pack-version "1.0")
+(live-pack-description "Neo 'soulhacker' Lee - customised font and color theme, and my own configuration merged from old .emacs file.")
