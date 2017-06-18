ee7d2e8d5f487716f4abb8a4e79cb6b9b1c5442f
Neo Lee <neo.lee@gmail.com>
2017-06-18 19:30:27 -0400

    Add compojure config

--- config/neo-conf.el
+++ config/neo-conf.el
@@ -87,3 +87,16 @@
 
 ;; Rainbow mode for nREPL as well
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+;; Clojure indentation config for compojure
+(require 'clojure-mode)
+
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
